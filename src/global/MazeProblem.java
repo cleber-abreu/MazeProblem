866664addb66a51394e268f1bfc6e95548d32901
cleber-abreu <clebjunio@gmail.com>
2016-08-24 13:44:15 -0300

    MazeProblem working now

--- src/global/MazeProblem.java
+++ src/global/MazeProblem.java
@@ -6,20 +6,27 @@ public class MazeProblem {
 	
 	private final static int[] H = {1, 0, -1,  0};
 	private final static int[] V = {0, 1,  0, -1};
-	private static int[][] table;
+	private int[][] fields;
+	private int height;
+	private int width;
 	
-	public MazeProblem(int iniRow, int iniCol, int size, int nBlocks) {
+	public MazeProblem(int iniRow, int iniCol, 
+			int endRow, int endCol,
+			int height, int width, 
+			int nBlocks) {
 
-		table = new int[size][size];
+		fields = new int[height][width];
+		this.height = height;
+		this.width = width;
 		
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				table[row][col] = 0;
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				fields[row][col] = 0;
 			}
 		}
 		
-		table[iniRow][iniCol] = 1;
-		table[size-1][size-1] = 2;
+		fields[iniRow][iniCol] = 2;
+		fields[endRow][endCol] = 3;
 		
 		insertBlocks(nBlocks);
 		
@@ -30,43 +37,43 @@ public class MazeProblem {
 		int row, col;
 		
 		for (int i = 0; i < n; i++) {
-			row = r.nextInt(table.length);
-			col = r.nextInt(table.length);
-			if (table[row][col] == 0) {
-				table[row][col] = -1;
+			row = r.nextInt(height);
+			col = r.nextInt(width);
+			if (fields[row][col] == 0) {
+				fields[row][col] = -1;
 			} else {
 				i--;
 			}
 		}
 	}
 	
-	public boolean tryOut(int row, int col) {
-		boolean done = (table[row][col] == 2);
+	public boolean tryOut(int row, int col, int i) {
 		int n = 0;
 		int r, c;
+		boolean done = (fields[row][col] == 3);
 		
 		while (!done && n < 4) {
 			r = row + H[n];
 			c = col + V[n];
 			
-			if (r >= 0 && r < table.length
-					&& c >= 0 && c < table.length
-					&& table[r][c] >= 0) {
+			if (r >= 0 && r < height
+					&& c >= 0 && c < width) {
 				
-				table[r][c] = 1;
-				done = tryOut(r, c);
-				
-//				if (!done) {
-//					table[r][c] = 0;
-//				}
+					if (fields[r][c] == 0) {
+						fields[r][c] = 1;
+						done = tryOut(r, c, i+1);
+					}				
+					else if (fields[r][c] == 3){
+						done = tryOut(r, c, i+1);
+					}
 			}
 			n++;
 		}
 		return done;
 	}
 
-	public int[][] getTable() {
-		return table;
+	public int[][] getFields() {
+		return fields;
 	}
 
 }
